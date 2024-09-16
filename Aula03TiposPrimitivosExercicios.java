6d7de6de5ecd1b623a31c74499e04b0fe22855d7
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-09-16 19:41:14 -0300

    Aula03 TiposPrimitivosExercicio
    Aula03  exercício de tipos primitivos em java

---
+++ Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args) {
+        //Exercicio01
+        String nome = "Matheus";
+        String endereco = "Rua Argentina 556";
+        float salario = 3500.00f;
+        String dataRecebimentoSalario = "06/09/2024"; //Como usar data e Java
+
+        System.out.println("Eu " + nome + " morando no endereço " + "\nconfirmo que recebi o " +
+                "salario de " + salario + " ,na data " + dataRecebimentoSalario);
+
+
+    }
+}
