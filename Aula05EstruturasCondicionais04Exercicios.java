8fbf4bf0d1c915b6f1b2be45298f7e202e27f007
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-09-18 13:01:27 -0300

    Update Aula05EstruturasCondicionais04Exercicios.java
    Colocando o valor total de imposto a ser pago

--- Aula05EstruturasCondicionais04Exercicios.java
+++ Aula05EstruturasCondicionais04Exercicios.java
@@ -8,7 +8,7 @@ De R$1.6903,99 até R$2.826,65	7,5%	R$142,80
 De R$2.826,66 até R$3.751,05	15%	R$354,80
 De R$3.751,06 até R$4.664,68	22,5%	R$636,13
          */
-        double salario = 15000.00;
+        double salario = 1700;
 
         double resSalario01 = 7.5 /100;
         double resTaxa01;
@@ -25,13 +25,16 @@ De R$3.751,06 até R$4.664,68	22,5%	R$636,13
 
         if (salario >= 1693.99 && salario <= 2826.65){
            resTaxa01 = salario - (salario * resSalario01);
-            System.out.println("Seu salario com a taxa de 7,5% fica no valor de: " + resSalario01);
+           valorImposto = salario * resSalario01;
+            System.out.println("Seu salario com a taxa de 7,5% fica no valor de: " + resTaxa01 + " pagando o total de imposto de " + valorImposto);
         } else if (salario >= 2826.66 && salario <= 3751.05) {
             resTaxa02 = salario - (salario * resSalario02);
-            System.out.println("Seu salario com a taxa de 15% fica no valor de: " +resTaxa02);
+            valorImposto = salario * resSalario02;
+            System.out.println("Seu salario com a taxa de 15% fica no valor de: " +resTaxa02 + " pagando o total de imposto de " + valorImposto);
         } else if (salario >= 3751.06) {
             resTaxa03 = salario -  (salario * resSalario03);
-            System.out.println("Seu salario com a taxa de 22,5% fica no valor de: " + resTaxa03);
+            valorImposto = salario * resSalario03;
+            System.out.println("Seu salario com a taxa de 22,5% fica no valor de: " + resTaxa03 + " pagando o total de imposto de " + valorImposto);
         } else {
             System.out.println("Esta isento de taxas");
         }
