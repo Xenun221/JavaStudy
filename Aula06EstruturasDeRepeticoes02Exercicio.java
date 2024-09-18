347a5357f174f59e6ef87b47ec45c02f35c683ad
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-09-18 16:24:38 -0300

    Aula06EstruturasDeRepeticoes02Exercicio
    Exercício usando o FOR de laços de repetições

---
+++ Aula06EstruturasDeRepeticoes02Exercicio.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratona.java.introducao;
+
+public class Aula06EstruturasDeRepeticoes02Exercicio {
+    public static void main(String[] args) {
+        //Imprima todos os numeros pares de 0 ate 1000000
+        for(int i =0;i <=1000000; i+=1){
+            if (i %2 ==0){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
