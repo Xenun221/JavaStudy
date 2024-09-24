c467de87dd4a3fa9f16ae90b41f318fb327695cf
Gabriel Nunes <101987835+Xenun221@users.noreply.github.com>
2024-09-24 09:25:47 -0300

    Aula01IntroducaoProgramacaoOrientadaAObjetopt4
    Aula Introdução a orientação a objeto

---
+++ Professor.java
@@ -0,0 +1,8 @@
+package academy.devdojo.maratona.javacore.Aintroducaoclasses.dominio;
+
+public class Professor {
+    //Não precisa especificar a variavel pois ela ja esta dentro de uma classe especifica e voce sabe no que esta mexendo
+    public String nome;
+    public int idade;
+    public  char sexo;
+}
